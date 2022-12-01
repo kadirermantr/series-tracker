8a0806ce524ea1417915ee558f1fd74c56126fa0
kadirerman <kadirermantr@gmail.com>
2022-12-02 00:49:50 +0300

    feat: add dashboard controller

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class DashboardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return View
+     */
+    public function index()
+    {
+        return view('dashboard');
+    }
+}
